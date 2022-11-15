19907aaf11a19e105938e39217b91fce00d38723
min2oyo <min2u@outlook.com>
2022-11-15 20:03:22 +0900

    My JSON Server 적용

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -11,7 +11,8 @@ const ProductAll = () => {
 
   // 함수
   const getProducts = async () => { // 상품 목록 API
-    let url = `http://localhost:3004/products?q=${query.get(`q`) || ``}`; // JSON Server에서 필터링
+    let url = `https://my-json-server.typicode.com/min2oyo/noona-react-hnm/products?q=${query.get(`q`) || ``}`; // JSON Server에서 필터링
+    // let url = `http://localhost:3004/products?q=${query.get(`q`) || ``}`; // JSON Server에서 필터링
     let data = await (await fetch(url)).json();
     console.log(`data: `, data);
     setProductlist(data);
